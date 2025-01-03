f8c5da452fc37271c15c56e0d43345d4b70cc153
Anurag Joshi <anuragj990@gmail.com>
2025-01-03 15:12:10 +0530

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,56 @@
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+ const initialList=[];
+
+ function makeInitalList()
+ {
+  for(let i=1;i<=50;i++)
+  {
+    let statusarr;
+    if(i%2!==0)
+    {
+      statusarr=[`To do ${i}`,true];
+    }
+    else
+    {
+      statusarr=[`To do ${i}`,false];
+    }
+    initialList.push(statusarr);
+  }
+ }
+
+ makeInitalList();
+
+ const [listarr, setListArr]=useState(initialList);
+
+ const findActive=useMemo(()=>{
+
+  const ActiveArr=initialList.filter((listitem)=> listitem[1]===false);
+  return ActiveArr;
+ },[initialList])
+
+ const findCompleted=useMemo(()=>{
+     const InactiveArr=initialList.filter((listitem)=> listitem[1]===true);
+     return InactiveArr;
+ },[initialList])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button onClick={()=> setListArr(initialList)}>All</button>
+        <button onClick={()=> setListArr(findActive)}>Active</button>
+        <button onClick={()=> setListArr(findCompleted)}>Completed</button>
+        <br />
+        <hr />
+        <p>Note: List is artificially slowed down!</p>
+        <br />
+        <ul>
+        {listarr.map((listitem, index)=> {return listitem[1]?<li key={index} style={{textDecoration: 'line-through'}}>{listitem[0]}</li>:<li key={index}>{listitem[0]}</li>})}
+        </ul>
     </div>
   )
 }
